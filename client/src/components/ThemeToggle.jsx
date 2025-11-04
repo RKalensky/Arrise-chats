392e9859c417fb15238cb063853cf9c52cc39477
roman kalenskyi <r.kalenskyi@easygenerator.com>
2025-11-04 17:08:42 +0200

    feat: draft theme toggle

---
+++ client/src/components/ThemeToggle.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Moon from '../assets/moon';
+import Sun from '../assets/sun';
+import * as styles from './ThemeToggle.module.scss';
+import classNames from 'classnames';
+
+export default function ThemeToggle() {
+    // light or dark
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
+    const toggleClasses = classNames(styles.toggle, {
+        [styles.light]: theme === 'light',
+        [styles.dark]: theme === 'dark',
+    })
+
+    const circleClasses = classNames(styles.circle, {
+        [styles.circleEndPosition]: theme === 'dark',
+    })
+
+    // TODO: add spring animations
+
+    return (
+        <div className={toggleClasses} onClick={toggleTheme}>
+            {theme === 'dark' && <Moon/>}
+            <div className={circleClasses}></div>
+            {theme === 'light' && <Sun/>}
+        </div>
+    )
+}
