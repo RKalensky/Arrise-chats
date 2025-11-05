4732abab41e29c271846419ed73d59d01d01d619
roman kalenskyi <r.kalenskyi@easygenerator.com>
2025-11-05 09:44:11 +0200

    chore: enable redux devtools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,22 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
 export default function initStore(initialStore = {}) {
-    const store = createStore(rootReducer, initialStore, applyMiddleware(sagaMiddleware));
-    sagaMiddleware.run(rootSaga);
+  const store = createStore(
+    rootReducer,
+    initialStore,
+    composeEnhancers(applyMiddleware(sagaMiddleware))
+  );
+  sagaMiddleware.run(rootSaga);
 
-    return store;
+  return store;
 }
-
