830a9326ae863e14e2295da78cd04bd863439615
roman kalenskyi <r.kalenskyi@easygenerator.com>
2025-11-06 01:39:16 +0200

    chore: color scheme

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import * as styles from './modal.module.scss';
 export default function Modal({ title, onClose, children }) {
   return (
     <div className={styles.modal}>
-      <h1>{title}</h1>
+      <h1 className={styles.title}>{title}</h1>
       <div className={styles.close} onClick={onClose} />
       <div className={styles.modalContent}>{children}</div>
     </div>
