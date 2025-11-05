664648bada33bb8c395d762faaaaf89333e1e33a
roman kalenskyi <r.kalenskyi@easygenerator.com>
2025-11-06 01:05:34 +0200

    chore: validation

--- client/src/utils/validateInputs.js
+++ client/src/utils/validateInputs.js
@@ -1,20 +1,26 @@
 const EMPTY_FIELD_ERROR = `Field can't be empty`;
-const TOO_LONG_FIELD_ERROR = (limit) => `Max ${limit} symbols are allowed`;
+const getTooLongFieldError = (limit) => `Max ${limit} symbols are allowed`;
 
 const MAX_MESSAGE_LENGTH = 255;
-const MAX_NAME_LENGTH = 40;
 
-const validate = (value, limit) => {
+const validateNotEmpty = (value) => {
   if (!value.trim()) {
     return EMPTY_FIELD_ERROR;
-  }
-
-  if (value.length > limit) {
-    return TOO_LONG_FIELD_ERROR(limit);
   }
 
   return '';
 };
 
-export const validateName = (name) => validate(name, MAX_NAME_LENGTH);
-export const validateMessage = (message) => validate(message, MAX_MESSAGE_LENGTH);
+const validateWithLimit = (value, limit) => {
+  const emptyError = validateNotEmpty(value);
+  if (emptyError) return emptyError;
+
+  if (value.length > limit) {
+    return getTooLongFieldError(limit);
+  }
+
+  return '';
+};
+
+export const validateName = (name) => validateNotEmpty(name);
+export const validateMessage = (message) => validateWithLimit(message, MAX_MESSAGE_LENGTH);
